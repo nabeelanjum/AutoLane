2f932084685bb245560b30be2dc09ff6f07a39b0
Nabeel Anjum <timedilation2012@gmail.com>
2024-12-22 15:53:38 +0500

    chore: navigation to map screen

--- src/components/CarStatusTile/index.tsx
+++ src/components/CarStatusTile/index.tsx
@@ -1,17 +1,21 @@
-import React, {useMemo} from 'react';
-import {View} from 'react-native';
+import React, {useCallback, useMemo} from 'react';
+import {Alert, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import {CarStatusItem} from '../../hooks/queries/types';
 import {statusStringMap} from './types';
 import AppText from '../shared/AppText';
 import AppButton from '../shared/AppButton';
 import colors from '../../common/colors';
 import useStyles from './styles';
+import {HomeRoutes} from '../../navigation/routes';
 
 interface Props {
   item: CarStatusItem;
 }
 
 const CarStatusTile: React.FC<Props> = ({item}) => {
+  const navigation = useNavigation();
+
   const isCarMoving = useMemo(
     () => item.status === 1 || item.status === 3,
     [item.status],
@@ -20,6 +24,14 @@ const CarStatusTile: React.FC<Props> = ({item}) => {
   const buttonLabel = useMemo(() => {
     return isCarMoving ? 'See on map' : 'Send car';
   }, [isCarMoving]);
+
+  const handleOnPress = useCallback(() => {
+    if (isCarMoving) {
+      navigation.navigate(HomeRoutes.LiveMap);
+    } else {
+      Alert.alert('Under Construction');
+    }
+  }, [isCarMoving, navigation]);
 
   const styles = useStyles(isCarMoving);
 
@@ -31,7 +43,11 @@ const CarStatusTile: React.FC<Props> = ({item}) => {
         </AppText>
         <AppText color={colors.fontSecondary}>{item.carNumber}</AppText>
       </View>
-      <AppButton style={styles.mapButton} label={buttonLabel} />
+      <AppButton
+        onPress={handleOnPress}
+        style={styles.mapButton}
+        label={buttonLabel}
+      />
     </View>
   );
 };
